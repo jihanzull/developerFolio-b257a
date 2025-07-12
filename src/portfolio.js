00581c0f5a0a54c97673373ffef3ce8e779070a1
jihanzull <nabilajihanzulfa@gmail.com>
2025-07-12 11:52:29 +0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,27 +20,25 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Jihan Zulfa Nabila",
+  title: "Hi all, I'm jihan",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1m_HkqNExtzKs0gOfaIkjA24FnHs_GrWl/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
-  // Instagram, Twitter and Kaggle are also supported in the links!
+  github: "https://github.com/jihanzull",
+  linkedin: "https://www.linkedin.com/in/jihan-zulfa-nabila-990b9324b/",
+  gmail: "nabilajihanzulfa@gmail.com",
+  instagram: "https://www.instagram.com/jihanzul_/",
+  tiktok: "https://www.tiktok.com/@jihanzul_?is_from_webapp=1&sender_device=pc",
+  // Instagram,Tiktok, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
 };
